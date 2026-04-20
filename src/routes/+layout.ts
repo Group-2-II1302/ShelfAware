4cb314091095b93c9092bb5c1bde77266b834f8f
arapg <araceli.prietogallego@weknowit.se>
2026-04-20 23:09:42 +0200

    feat: added a server layout

---
+++ src/routes/+layout.ts
@@ -0,0 +1,44 @@
+import {
+  PUBLIC_SUPABASE_PUBLISHABLE_KEY,
+  PUBLIC_SUPABASE_URL,
+} from '$env/static/public'
+import {
+  createBrowserClient,
+  createServerClient,
+  isBrowser,
+} from '@supabase/ssr'
+import type { LayoutLoad } from './$types'
+
+export const load: LayoutLoad = async ({ fetch, data, depends }) => {
+  depends('supabase:auth')
+
+  const supabase = isBrowser()
+    ? createBrowserClient(
+        PUBLIC_SUPABASE_URL,
+        PUBLIC_SUPABASE_PUBLISHABLE_KEY,
+        {
+          global: {
+            fetch,
+          },
+        },
+      )
+    : createServerClient(
+        PUBLIC_SUPABASE_URL,
+        PUBLIC_SUPABASE_PUBLISHABLE_KEY,
+        {
+          global: {
+            fetch,
+          },
+          cookies: {
+            getAll() {
+              return data.cookies
+            },
+          },
+        },
+      )
+
+  const { data: claimsData, error } = await supabase.auth.getClaims()
+  const claims = error ? null : claimsData?.claims
+
+  return { supabase, claims }
+}
