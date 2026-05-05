6a308c58303073747239704cf58200b8b0403bd8
GD-lost-in-code <geoannadahore@gmail.com>
2026-05-05 20:35:05 +0200

    chore: prettier formatting

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,16 +1,16 @@
-import type { PageServerLoad } from './$types'
+import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async ({ locals }) => {
   const { data: shelves, error } = await locals.supabase
-    .from('shelves')
-    .select('id, name')
-    .order('name')
+    .from("shelves")
+    .select("id, name")
+    .order("name");
 
   if (error) {
-    throw error
+    throw error;
   }
 
   return {
     shelves,
-  }
-}
+  };
+};
