fbb22dcfc111b327b03db090a648a7587d384cee
GD-lost-in-code <geoannadahore@gmail.com>
2026-05-05 20:32:28 +0200

    refactor: Homepage renders the shelves in the DB androuting is done so each shelf sends to corresponding view

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,21 @@
+<script lang="ts">
+  import type { PageData } from './$types'
+
+  let { data }: { data: PageData } = $props()
+</script>
+
 <h1>welcome to shelfAware</h1>
 <p>your shelves</p>
 
 <ul class="shelf-list">
-  <li class="shelf-list__item">
-    <a href="/fridge" class="shelf-list__link">
-      fridge
-    </a>
-  </li>
-
-  <li class="shelf-list__item">
-    <a href="/pantry" class="shelf-list__link">
-      pantry
-    </a>
-  </li>
+  {#each data.shelves as shelf (shelf.id)}
+    <li class="shelf-list__item">
+      <a
+        href="/shelves/{shelf.id}"
+        class="shelf-list__link"
+      >
+        {shelf.name}
+      </a>
+    </li>
+  {/each}
 </ul>
